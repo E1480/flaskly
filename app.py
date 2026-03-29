c21c0037d08044227ab01dee8090e32ac8434ce8
E1480 <that1480deguy@gmail.com>
2026-03-29 23:06:22 +0800

    Inital

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def _root():
+    return jsonify({"status":"OK"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
